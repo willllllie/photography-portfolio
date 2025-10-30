28c86acc92e0f8ba7bec5f3ac214604dbb93c2d8
Willie Franklin <williefranklin@Mac.mynet>
2025-10-30 20:47:04 +0000

    cursor: empty commit to allow worktree creation

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./public/**/*.html",
+    "./src/**/*.js"
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+};
+
+
